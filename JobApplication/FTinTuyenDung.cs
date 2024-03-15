a75cb4c8b9f62abfbcfecad2b183ab43154247b4
VAKhoa939 <21110046@student.hcmute.edu.vn>
2024-03-15 10:47:04 +0700

    Update Delete files and Rename files 2

---
+++ JobApplication/FTinTuyenDung.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JobApplication
+{
+    public partial class FTinTuyenDung : Form
+    {
+        public FTinTuyenDung()
+        {
+            InitializeComponent();
+        }
+
+        private void TinTuyenDung_Load(object sender, EventArgs e)
+        {
+            UCBaiDang uCBaiDang = new UCBaiDang();
+
+        }
+    }
+}
